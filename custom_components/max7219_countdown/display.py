d2e8dd78dbe7c6fb4c979fdb6b5ca4a74dead226
Martijn Donath <martijn.donath@gmail.com>
2021-11-08 13:26:00 +0100

    custom_components

---
+++ custom_components/max7219_countdown/display.py
@@ -0,0 +1,49 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import display, spi
+from esphome.const import CONF_ID, CONF_INTENSITY, CONF_LAMBDA, CONF_NUM_CHIPS, CONF_INDEX
+
+# AUTO_LOAD = ["max7219"]
+
+DEPENDENCIES = ["spi"]
+
+max7219_ns = cg.esphome_ns.namespace("max7219")
+MAX7219CountdownComponent = max7219_ns.class_(
+    "MAX7219CountdownComponent", cg.PollingComponent, spi.SPIDevice
+)
+MAX7219CountdownComponentRef = MAX7219CountdownComponent.operator("ref")
+
+CONF_REVERSE_ENABLE = "reverse_enable"
+
+CONFIG_SCHEMA = (
+    display.BASIC_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(MAX7219CountdownComponent),
+            cv.Optional(CONF_NUM_CHIPS, default=1): cv.int_range(min=1, max=255),
+            cv.Optional(CONF_INTENSITY, default=15): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_REVERSE_ENABLE, default=False): cv.boolean,
+            cv.Optional(CONF_INDEX): cv.All([cv.int_], cv.Length(min=16, max=16)),
+        }
+    )
+    .extend(cv.polling_component_schema("1s"))
+    .extend(spi.spi_device_schema())
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)
+    await display.register_display(var, config)
+
+    cg.add(var.set_num_chips(config[CONF_NUM_CHIPS]))
+    cg.add(var.set_intensity(config[CONF_INTENSITY]))
+    cg.add(var.set_reverse(config[CONF_REVERSE_ENABLE]))
+    if CONF_INDEX in config:
+        cg.add(var.set_mapping(config[CONF_INDEX]))
+
+    if CONF_LAMBDA in config:
+        lambda_ = await cg.process_lambda(
+            config[CONF_LAMBDA], [(MAX7219CountdownComponentRef, "it")], return_type=cg.void
+        )
+        cg.add(var.set_writer(lambda_))
---
+++ custom_components/max7219_countdown/max7219_countdown.h
@@ -0,0 +1,67 @@
+#pragma once
+ 
+#include "esphome/core/component.h"
+#include "esphome/core/defines.h"
+ 
+#ifdef USE_TIME
+#include "esphome/components/time/real_time_clock.h"
+#endif
+ 
+#include "esphome/components/spi/spi.h"
+ 
+namespace esphome {
+namespace max7219 {
+ 
+class MAX7219CountdownComponent;
+ 
+using max7219countdown_writer_t = std::function<void(MAX7219CountdownComponent &)>;
+ 
+ class MAX7219CountdownComponent : public PollingComponent,
+                                   public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW,
+                                                         spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1MHZ> {
+  public:
+   void set_mapping(std::initializer_list<uint8> mapping);
+
+   void set_writer(max7219countdown_writer_t &&writer);
+ 
+   void setup() override;
+ 
+   void dump_config() override;
+ 
+   void update() override;
+ 
+   float get_setup_priority() const override;
+ 
+   void display();
+ 
+   void set_intensity(uint8_t intensity);
+   void set_num_chips(uint8_t num_chips);
+   void set_reverse(bool reverse) { this->reverse_ = reverse; };
+ 
+   uint8_t printf(uint8_t pos, const char *format, ...) __attribute__((format(printf, 3, 4)));
+   uint8_t printf(const char *format, ...) __attribute__((format(printf, 2, 3)));
+ 
+   uint8_t print(uint8_t pos, const char *str);
+   uint8_t print(const char *str);
+ 
+ #ifdef USE_TIME
+   uint8_t strftime(uint8_t pos, const char *format, time::ESPTime time) __attribute__((format(strftime, 3, 0)));
+ 
+   uint8_t strftime(const char *format, time::ESPTime time) __attribute__((format(strftime, 2, 0)));
+ #endif
+ 
+  protected:
+   void send_byte_(uint8_t a_register, uint8_t data);
+   void send_to_all_(uint8_t a_register, uint8_t data);
+ 
+   uint8_t intensity_{15};  
+   uint8_t num_chips_{1};
+   uint8_t *buffer_;
+   bool reverse_{false};
+   optional<max7219countdown_writer_t> writer_{};
+   bool has_map_{false};
+   uint8_t map_[16];
+ };
+ 
+ }  // namespace max7219
+ }  // namespace esphome
